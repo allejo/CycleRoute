9617a19bf9cf9681d064a4ca5e619dd4d2bc8560
xocaid <xocaid@gmail.com>
2022-11-22 22:01:59 -0800

    Notes Section Complete

--- CycleRoute/client/src/components/Tabs/Favorites/favCard.js
+++ CycleRoute/client/src/components/Tabs/Favorites/favCard.js
@@ -5,10 +5,11 @@ import NotFavButton from './notFavButton';
 function FavoriteCard({ oneFavCard, id, deleteFavorite }) {
   const [dropDown, setDropDown] = useState(true);
   const [notes, setNotes] = useState('');
+  const [editNotes, setEditNotes] = useState(true);
 
-//This useEffect is passing the information from favorite.js with prop oneFavCard
-//If dependecy array(2nd parameter []) has changed since the last render, will the effect be rerun if it changes.
-//Didn't set notes in here because it is a state variable, if notes were in the dependecy array it would trigger a rerun, resulting in an infinite loop.
+  //This useEffect is passing the information from favorite.js with prop oneFavCard
+  //If dependecy array(2nd parameter []) has changed since the last render, will the effect be rerun if it changes.
+  //Didn't set notes in here because it is a state variable, if notes were in the dependecy array it would trigger a rerun, resulting in an infinite loop.
   useEffect(() => {
     setNotes(oneFavCard.notes);
   }, [oneFavCard.notes]);
@@ -25,6 +26,10 @@ function FavoriteCard({ oneFavCard, id, deleteFavorite }) {
 
   const handleDropDown = () => {
     setDropDown(!dropDown);
+  };
+
+  const toggleEditNotes = () => {
+    setEditNotes(!editNotes);
   };
 
 
@@ -48,26 +53,30 @@ function FavoriteCard({ oneFavCard, id, deleteFavorite }) {
             <h2>Distance: <span className='favcard-results-span'>{oneFavCard.distance}</span></h2>
             <h2>Duration: <span className='favcard-results-span'>{oneFavCard.duration}</span></h2>
           </div>
+          {editNotes ?
+            (<div className='notes-section'>
+              <h2 >Notes: <span className='favcard-results-span'>{oneFavCard.notes}</span></h2>
+              <br/>
+              <button onClick={toggleEditNotes}>Edit Notes</button>
+            </div>
+            ) : (
 
-          <div className='notes-section'>
-            <h2 >Notes: <span className='favcard-results-span'>{oneFavCard.notes}</span></h2>
-          </div>
+              <div>
 
-
-          <div>
-            <textarea
-              className='notes-textarea'
-              type="text"
-              id="add-notes"
-              placeholder="Add your notes here..."
-              value={notes}
-              onChange={(e) => setNotes(e.currentTarget.value)}
-            />
-            <br />
-            <button onClick={editFavorite}>Add Notes</button>
-          </div>
-
-          <NotFavButton id={id} deleteFavorite={deleteFavorite} />
+                <textarea
+                  className='notes-textarea'
+                  type="text"
+                  id="add-notes"
+                  placeholder="Add your notes here..."
+                  value={notes}
+                  onChange={(e) => setNotes(e.currentTarget.value)}
+                />
+                <br />
+                <button onClick={editFavorite}>Update Notes</button>
+                <button onClick={toggleEditNotes}>Close Notes</button>
+              </div>
+            )}
+            < NotFavButton id={id} deleteFavorite={deleteFavorite} />
         </div>
       )}
 
